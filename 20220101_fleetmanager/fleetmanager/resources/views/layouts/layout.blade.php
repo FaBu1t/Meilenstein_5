3dbcbb9a4fc5226287f94615c71d237519a70bfa
fbuitkam <fabian.buitkamp@hs-osnabrueck.de>
2022-01-16 18:03:01 +0100

    Aufgabe 4

--- 20220101_fleetmanager/fleetmanager/resources/views/layouts/layout.blade.php
+++ 20220101_fleetmanager/fleetmanager/resources/views/layouts/layout.blade.php
@@ -13,7 +13,25 @@
     <title>Schiffsdatenbank</title>
   </head>
   <body>
-    @yield('navbar')
+    <nav class="navbar navbar-expand-lg navbar-light bg-light">
+    <a class="navbar-brand" href="http://127.0.0.1:8000/dashboard/">Flottenmanagement</a>
+    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarNav">
+      <ul class="navbar-nav">
+        <li class="nav-item">
+          <a class="nav-link" href="/ships/">Schiffe</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="/manufacturers/">Hersteller</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="/miniships/">Schiffsmodelle</a>
+        </li>
+    </ul>
+  </div>
+</nav>
     <form method="post" action="{{url('logout')}}">
       @csrf
       <input type="submit" class="btn btn-danger" value="Abmelden">
